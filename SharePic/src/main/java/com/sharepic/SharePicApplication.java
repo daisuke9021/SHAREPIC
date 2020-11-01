95e1f4e8ccadc4d26c9d378ef64dd5eda041458c
Daisuke Takakuwa <66095465+daisuke9021@users.noreply.github.com>
2020-11-01 12:28:12 +0900

    新規作成

---
+++ SharePic/src/main/java/com/sharepic/SharePicApplication.java
@@ -0,0 +1,13 @@
+package com.sharepic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharePicApplication.class, args);
+	}
+
+}
---
+++ SharePic/src/test/java/com/sharepic/SharePicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharepic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharePicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
